a635fa7cb33f87dd3112ca7370efef03e0c72e23
Victor <vb4007@gmail.com>
2017-11-16 16:28:21 +0100

    More tests

--- test/test-wip.js
+++ test/test-wip.js
@@ -10,10 +10,12 @@ let webhook = JSON.parse(content)
 
 let server = require('../index.js')
 
+let wip = require('../src/wip-status')
+
 chai.use(chaiHttp);
 
 describe('WIP pull request status', function () {
-        it("should set a success status when PR is opened", (done) => {
+        it("should detect when a pull request is opened", (done) => {
             chai.request(server)
             .post('/api')
             .send(webhook)
@@ -22,6 +24,33 @@ describe('WIP pull request status', function () {
                 res.text.should.equals('this is a pr')
                 done()
             })
+        })
+
+        it("should send a correct body", (done) => {
+            var response = wip.wipStatus(webhook.body)
+            //console.log(response)
+            response.should.have.property("body")
+
+            response.body.should.have.property("state")
+            response.body.should.have.property("target_url")
+            response.body.should.have.property("description")
+            response.body.should.have.property("context")
+
+            response.body.state.should.equals("success")
+
+            done()
+        })
+
+        it("should send it to the correct url", (done) => {
+            process.env.NODE_ENV = 'production'
+
+            var response = wip.wipStatus(webhook)
+            response.should.have.property("url")
+            response.url.should.be.equals("https://api.github.com/repos/Raul6469/snapcaster/statuses/058a6f46acf879762b6b6d35f3c06f4ae4f884c5")
+            
+            process.env.NODE_ENV = 'test'
+
+            done()
         })
     }
 )
